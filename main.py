9fd3975d38cae55963528d59372e55f969c21434
linneafairy <21lleaver@castilleja.org>
2022-11-14 16:51:42 -0500

    actual first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#################################################
+# term project fall '22: skincare simulator
+#
+# Your name: Linnea
+# Your andrew id: lleaver
+#################################################
+
+import math
+import os
+import copy
+import decimal
